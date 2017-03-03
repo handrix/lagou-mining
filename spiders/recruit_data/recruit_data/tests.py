cbbc802b509173f5d986b4824bae1a938fcb04fb
handrix <zequnfeng@gmail.com>
2017-03-03 10:40:10 +0800

    撰写测试用例

--- spiders/recruit_data/recruit_data/tests.py
+++ spiders/recruit_data/recruit_data/tests.py
@@ -1,10 +1,19 @@
+# -*- coding:utf-8 -*-
+
 import unittest
+import os
+import sys
 
+class RecruiteSpiderTest(unittest.TestCase):
+    def setUp(self):
+        project_root = __file__.rsplit('/', 2)[0]
+        os.chdir(project_root)
+        self.client = os.path.join(os.path.dirname(sys.executable), 'scrapy')
+        pass
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
-
+    def lagou_test(self):
+        os.system('{} crawl lagou -s DOWNLOAD_DELAY={}'.format(self.client, '1'))
+        pass
 
 if __name__ == '__main__':
     unittest.main()
